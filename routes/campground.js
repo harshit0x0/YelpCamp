f85657131f2bc9a0b39205101ed4f8e028b0c488
harshit0x0 <hpandey8888@gmail.com>
2024-04-04 23:54:13 +0530

    added authorization for campgrounds

--- routes/campground.js
+++ routes/campground.js
@@ -1,24 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-const ExpressError = require('../utils/ExpressErrors');
 const Campground = require("../models/campground");
-const { campgroundSchema } = require('../schemas.js');
 const methodOverride = require('method-override');
-const { isLoggedin } = require('../utils/middlewares');
+const { isLoggedin, validateCampground, isAuthor } = require('../utils/middlewares');
 
 router.use(methodOverride('_method'));
-
-const validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body);
-    if (error) {
-        const message = error.details.map(el => el.message).join(',');
-        throw new ExpressError(message, 400)
-    }
-    else {
-        next();
-    }
-}
 
 router.get('/', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -32,14 +19,12 @@ router.get('/new', isLoggedin, (req, res) => {
 router.get('/:id/edit', isLoggedin, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
-    console.log(campground);
     res.render("campgrounds/edit.ejs", { campground });
 }))
 
 router.get('/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id).populate('reviews').populate('author');
-    console.log(campground);
     if (!campground) {
         req.flash('error', "Cannot find that campground!");
         return res.redirect('/campgrounds');
@@ -47,7 +32,7 @@ router.get('/:id', catchAsync(async (req, res) => {
     res.render("campgrounds/show.ejs", { campground });
 }))
 
-router.put('/:id', isLoggedin, validateCampground, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedin, isAuthor, validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
     if (!campground) {
@@ -65,8 +50,8 @@ router.post('/', isLoggedin, validateCampground, catchAsync(async (req, res) => 
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
-router.delete("/:id", isLoggedin, catchAsync(async (req, res) => {
-    const { id } = req.params
+router.delete("/:id", isLoggedin, isAuthor, catchAsync(async (req, res) => {
+    const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     req.flash('success', "Succefully deleted the campground!");
     res.redirect(`/campgrounds`)
--- utils/middlewares.js
+++ utils/middlewares.js
@@ -1,8 +1,10 @@
+const { campgroundSchema } = require('../schemas.js');
+const ExpressError = require('./ExpressErrors');
+const Campground = require("../models/campground");
+
 module.exports.storeReturnTo = (req, res, next) => {
-    console.log('inside middleware');
     if (req.session.returnTo) {
         res.locals.returnTo = req.session.returnTo;
-        // console.log(res.locals.returnTo);
     }
     next();
 }
@@ -12,6 +14,28 @@ module.exports.isLoggedin = (req, res, next) => {
         req.session.returnTo = req.originalUrl;
         req.flash('error', "You must be logged in first!")
         return res.redirect('/login');
+    }
+    next();
+}
+
+
+module.exports.validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const message = error.details.map(el => el.message).join(',');
+        throw new ExpressError(message, 400)
+    }
+    else {
+        next();
+    }
+}
+
+module.exports.isAuthor = async (req, res, next) => {
+    const { id } = req.params;
+    const campground1 = await Campground.findById(id);
+    if (!req.user || !campground1.author._id.equals(req.user._id)) {
+        req.flash('error', "You don't have permission to do that");
+        return res.redirect(`/campgrounds/${id}`);
     }
     next();
 }

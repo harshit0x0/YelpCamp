da5d0f7e3f29f2bf4f3f0f368c63016fb46a87c0
harshit0x0 <hpandey8888@gmail.com>
2024-04-12 10:55:33 +0530

    fixed minor bugs

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -10,12 +10,13 @@ const campgrounds = (campgroundData.features);
 
 const markers = L.markerClusterGroup();
 for (let campground of campgrounds) {
-    const newMarker = L.marker(new L.latLng(campground.lat, campground.lon));    
+    const newMarker = L.marker(new L.latLng(campground.lat, campground.lon));  
+    const imageUrl = campground.images.length ? campground.images[0].url : "https://res.cloudinary.com/dt1kuh1tm/image/upload/v1712898501/augustine-wong-T0BYurbDK_M-unsplash_teqbzz.jpg";  
     popupContent = 
         `
         <a class="popUpLink" href="campgrounds/${campground._id}"> ${campground.title} </a>
         <p class="popUpLocation"> ${campground.location} </p>
-        <img class="popUpImg" src = ${campground.images[0].url.replace('/upload', '/upload/w_200') }>
+        <img class="popUpImg" src = ${imageUrl.replace('/upload', '/upload/w_200') }>
         `;
 
     newMarker.bindPopup(popupContent);
